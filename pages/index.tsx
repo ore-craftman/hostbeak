71e636dc74e0b9ada8964c6dfb570ed4a83eece0
Oreoluwa <ore.craftman@gmail.com>
2022-04-11 11:34:48 +0100

    Updated base route

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,37 +12,36 @@ const Home: NextPage = () => {
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+          Hi, I'm{' '}
+          <a className="text-blue-700" href="https://nextjs.org">
+            Oreoluwa
           </a>
         </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+        <p className="mt-3 text-2xl">Ogoluwa's Front-end test for HostGeak</p>
 
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
           <a
-            href="https://nextjs.org/docs"
+            href="/sign-up"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
+            <h3 className="text-2xl font-bold">Sign Up Page &rarr;</h3>
             <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
+              Click here to see the{' '}
+              <code className="rounded-md bg-gray-200 p-2">Sign Up</code> UI
+              implementation.
             </p>
           </a>
 
           <a
-            href="https://nextjs.org/learn"
+            href="/add-bill"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
+            <h3 className="text-2xl font-bold">Add Bill Page &rarr;</h3>
             <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
+              Click here to see the{' '}
+              <code className="rounded-md bg-gray-200 p-2">Add Bill</code> UI
+              implementation.
             </p>
           </a>
 
@@ -50,19 +49,22 @@ const Home: NextPage = () => {
             href="https://github.com/vercel/next.js/tree/canary/examples"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
+            <h3 className="text-2xl font-bold">View Reports Page &rarr;</h3>
             <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
+              Click here to see the{' '}
+              <code className="rounded-md bg-gray-200 p-2">Reports</code> UI
+              implementation.
             </p>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://github.com/ore-craftman/hostbeak"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
+            <h3 className="text-2xl font-bold">See My Code &rarr;</h3>
             <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Before the talks get too much.. Check see{' '}
+              <code className="my-2 rounded-md bg-gray-200 p-2">my code</code>
             </p>
           </a>
         </div>
@@ -71,12 +73,12 @@ const Home: NextPage = () => {
       <footer className="flex h-24 w-full items-center justify-center border-t">
         <a
           className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="mailto:ore.craftman@gmail.com"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          <code className="rounded-md bg-gray-200 p-2">{'<Oreoluwa />'}</code>
         </a>
       </footer>
     </div>
